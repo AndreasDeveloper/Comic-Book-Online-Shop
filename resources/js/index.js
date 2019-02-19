33b2389c1807fb0e3acaaff89abac683df072cf5
AndreasDEV <andreasmrdja16@gmail.com>
2019-02-20 00:07:36 +0100

    Built shopping cart on hover. RFI. | Update #6 |

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,7 +1,7 @@
 // * --- WEBPACK IMPORT FILES --- * \\
 import '../sass/main.scss';
 
-// -- SIDE NAVIGATION | - Logic -- \\
+// ** --- SIDE NAVIGATION | - Logic --- ** \\
 (() => {
     // DOM Elements
     const navigation = document.querySelector('.nav-toggle');
@@ -13,4 +13,30 @@ import '../sass/main.scss';
         html.classList.toggle('openNav');
         navigation.classList.toggle('active');
     });
+})();
+
+// ** --- IMAGE OBSERVING | OPTIMIZATION | LAZY LOAD --- ** \\
+(() => {
+    // DOM Element
+    const images = document.querySelectorAll('img');
+    // - Function | - For lazy loading images
+    const lazyLoad = target => {
+        const io = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                console.log('Intersecting');
+                // If image is intersecting, run the code and load the image
+                if (entry.isIntersecting) {
+                    const img = entry.target;
+                    const src = img.getAttribute('data-lazy');
+                    // Setting attribute to src and adding fade class (effect/transition)
+                    img.setAttribute('src', src);
+                    img.classList.add('fade');
+                    // After everything is done, observer will disconnect
+                    observer.disconnect();
+                }
+            });
+        });
+        io.observe(target); // Calling observer on targets
+    };
+images.forEach(lazyLoad); // Lazy load every single img in HTML
 })();
